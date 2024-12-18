1a3feb3b9a05f2cf7af08aef5152892b73df490a
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2024-12-18 08:35:49 +0000

    Frontend Projet
    Gestion des projets

--- src/Controller/Frontend/ProjetController.php
+++ src/Controller/Frontend/ProjetController.php
@@ -5,15 +5,22 @@ declare(strict_types=1);
 namespace App\Controller\Frontend;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/projet')]
 class ProjetController extends AbstractController
 {
-    #[Route('/tbord', name: 'app_frontend_projet_tbord')]
+    #[Route('/', name: 'app_frontend_projet_tbord')]
     public function tbord(): Response
     {
         return $this->render('frontend/projet_tbord.html.twig');
+    }
+
+    #[Route('/add', name: 'app_frontend_projet_add', methods: ['GET','POST'])]
+    public function add(Request $request)
+    {
+        return $this->render('frontend/projet_add.html.twig');
     }
 }
